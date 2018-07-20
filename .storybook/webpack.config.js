e73785839433930c55b8859b5e43d060fba1881b
Michael Woo <mwoo123@gmail.com>
2018-07-20 17:08:06 -0400

    initial commit

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules.push({
+    test: /\.css$/,
+    exclude: [
+      path.resolve(__dirname, '../node_modules'),
+    ],
+    use: [
+      require.resolve('style-loader'),
+      {
+        loader: require.resolve('css-loader'),
+        options: {
+          importLoaders: 1,
+          modules: true,
+          localIdentName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+      {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          // Necessary for external CSS imports to work
+          // https://github.com/facebookincubator/create-react-app/issues/2677
+          ident: 'postcss',
+          plugins: () => [
+            require('postcss-flexbugs-fixes'),
+            autoprefixer({
+              browsers: [
+                '>1%',
+                'last 4 versions',
+                'Firefox ESR',
+                'not ie < 9', // React doesn't support IE8 anyway
+              ],
+              flexbox: 'no-2009',
+            }),
+          ],
+        },
+      },
+    ],
+  });
+
+  return defaultConfig;
+};
