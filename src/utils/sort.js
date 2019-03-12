63558d38a524de2e030b858c61dc43c9cba0ea53
Jason <jason412389806@hotmail.com>
2019-03-12 17:03:14 -0400

    bug/handle-empty-data

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -17,7 +17,7 @@ const sortBy = (type, dirFactor) => (a, b) => {
   return 0
 }
 
-export default (type, direction) => {
+const sort = (type, direction) => {
   const dirFactor = direction === 'ascending' ? 1 : -1
   switch (type) {
   case 'string':
@@ -38,4 +38,28 @@ export default (type, direction) => {
       return dirFactor * (a - b)
     }
   }
+}
+
+const getDefaultSortType = (data, key) => {
+  if (data.length === 0) {
+    return null
+  }
+
+  const value = data[0][key]
+  if ([null, undefined, NaN].includes(value)) {
+    return null
+  }
+
+  if (Number.isInteger(value)) {
+    return 'basic'
+  } else if (Number.isInteger(Date.parse(value))) {
+    return 'date'
+  } else {
+    return 'string'
+  }
+}
+
+export {
+  sort,
+  getDefaultSortType,
 }
